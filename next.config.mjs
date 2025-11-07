addb16b2ea6f0dfa5a6fbfd598daff33b10d458b
M Lohith <lohibunny@gmail.com>
2025-11-07 10:49:19 +0530

    Fix: Downgrade to puppeteer-core@10.4.0, add ignore-loader for source maps, reduce memory to 1024MB

--- next.config.mjs
+++ next.config.mjs
@@ -9,6 +9,21 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+  webpack: (config, { isServer }) => {
+    // Exclude source maps from chrome-aws-lambda
+    config.module.rules.push({
+      test: /\.map$/,
+      use: 'ignore-loader'
+    });
+    
+    // Exclude chrome-aws-lambda source maps from being parsed
+    config.module.rules.push({
+      test: /node_modules[\\/]chrome-aws-lambda[\\/].*\.js\.map$/,
+      loader: 'ignore-loader'
+    });
+
+    return config;
+  },
 }
 
 export default nextConfig
